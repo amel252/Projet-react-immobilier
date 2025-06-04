9573e2953be61e29548cb4effeaba44ee4f6dbb3
Amel252 <amelghanem25@gmail.com>
2025-06-04 17:10:41 +0200

    Création l'Api de l'inscription

--- back-end/index.js
+++ back-end/index.js
@@ -2,10 +2,14 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 // dotenv on l'appelle avec ca
+
 import userRouter from "./routes/user.route.js";
 dotenv.config();
 
+import authRouter from "./routes/auth.route.js";
+
 const app = express();
+app.use(express.json());
 mongoose
     .connect(process.env.MONGO)
     .then(() => {
@@ -17,5 +21,7 @@ mongoose
 app.listen(3000, () => {
     console.log("le serveur est écouté sur le port 3000 ? ");
 });
+
 // import des routes
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
