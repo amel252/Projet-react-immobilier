16d3a28145043d0d72e2bd615c2448acc8fd8eab
Amel252 <amelghanem25@gmail.com>
2025-06-04 15:19:49 +0200

    connexion à la BDD

--- back-end/index.js
+++ back-end/index.js
@@ -1,5 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+// dotenv on l'appelle avec ca
+dotenv.config();
+
 const app = express();
+mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+        console.log("Connected to BD");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 app.listen(3000, () => {
     console.log("le serveur est écouté sur le port 3000 ? ");
 });
--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -21,8 +21,6 @@ function App() {
             </Routes>
         </BrowserRouter>
     );
-
-    // return <div className="text-center bg-blue-700 text-white py-3">App</div>;
 }
 
 export default App;
