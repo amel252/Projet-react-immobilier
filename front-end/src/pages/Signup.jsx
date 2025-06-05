c4e03021680ccd5ef1054474aa2a1d8deb890877
Amel252 <amelghanem25@gmail.com>
2025-06-05 14:53:12 +0200

    Création formulaire d'inscription

--- front-end/src/pages/Signup.jsx
+++ front-end/src/pages/Signup.jsx
@@ -1,4 +1,41 @@
+import { Link } from "react-router-dom";
 function Signup() {
-    return <div>Signup</div>;
+    return (
+        <div className="p-3 max-w-lg mx-auto">
+            <h1 className="text-3xl text-center font-semibold my-7">
+                Inscription
+            </h1>
+            <form className="flex flex-col gap-4">
+                <input
+                    type="text"
+                    className="border p-3 rounded-lg"
+                    placeholder="Nom"
+                    // on doit faire attention nomination sur le back doit etre la meme ici
+                    id="username"
+                />
+                <input
+                    type="text"
+                    className="border p-3 rounded-lg"
+                    placeholder="email"
+                    id="email"
+                />
+                <input
+                    type="text"
+                    className="border p-3 rounded-lg"
+                    placeholder="password"
+                    id="password"
+                />
+                <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+                    Inscrivez-vous
+                </button>
+            </form>
+            <div className="flex gap-2 mt-5">
+                <p>Avez vous déja un compte ?</p>
+                <Link to={"/sign-in"}>
+                    <span className="text-blue-700">Connectez-vous</span>
+                </Link>
+            </div>
+        </div>
+    );
 }
 export default Signup;
