f38b667d3fa3689db3b111ae40f048bbe70011c1
Amel252 <amelghanem25@gmail.com>
2025-06-09 16:18:33 +0200

    Ajout de redux persist pour garder les données de l'utilisateur

--- front-end/src/redux/store.js
+++ front-end/src/redux/store.js
@@ -1,10 +1,25 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import useReducer from "./user/userSlice.js";
 
+// combiner mes reducers :
+const rootReducer = combineReducers({ user: useReducer });
+// configuration de persist :
+const persistConfig = {
+    key: "root",
+    storage,
+    version: 1,
+};
+// persist reducers
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-    reducer: { user: useReducer },
+    reducer: persistedReducer,
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware({
             serializableCheck: false,
         }),
 });
+
+export const persistor = persistStore(store);
