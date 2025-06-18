4b97652b652705d1024aa5fbf80679054892670b
Amel252 <amelghanem25@gmail.com>
2025-06-18 14:25:21 +0200

    l'API permettant à la création des annonces (Listing)

--- back-end/index.js
+++ back-end/index.js
@@ -9,6 +9,7 @@ dotenv.config();
 
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
 
 const app = express();
 app.use(express.json());
@@ -29,6 +30,7 @@ app.listen(3000, () => {
 // import des routes
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
 // creation de middleware
 app.use((err, req, res, next) => {
