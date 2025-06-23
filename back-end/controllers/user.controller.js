0449fbee0ce52ef2dca79b3eb2690747ec1dc9ac
Amel252 <amelghanem25@gmail.com>
2025-06-23 15:09:17 +0200

    Création de l'API pemettant a l'utilisateur de récupere les annonces a partir de son id

--- back-end/controllers/user.controller.js
+++ back-end/controllers/user.controller.js
@@ -1,6 +1,9 @@
 import { errorHandler } from "../utils/errors.js";
 import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
+import Listing from '../models/listing.model.js'; // assure-toi d'importer ton modèle
+
+
 export const test = (req, res) => {
     res.json({
         message: "Api  de test fonctionne",
@@ -58,3 +61,17 @@ export const deleteUser = async (req, res, next) => {
         next(error);
     }
 }
+
+export const getUserListings = async (req, res, next) => {
+    if (req.user.id === req.params.id) {
+        try {
+            // Récupère les annonces de l'utilisateur
+            const listings = await Listing.find({ userRef: req.params.id });
+            res.status(200).json(listings);
+        } catch (error) {
+            next(error);
+        }
+    } else {
+        return next(errorHandler(401, "Tu peux voir que tes propres annonces."));
+    }
+};
--- back-end/routes/user.route.js
+++ back-end/routes/user.route.js
@@ -2,7 +2,7 @@ import express from "express";
 import {
     test,
     updateUser,
-    deleteUser,
+    deleteUser,getUserListings
 } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -12,5 +12,6 @@ const router = express.Router();
 router.get("/test", test);
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
+router.get("/listings/:id", verifyToken, getUserListings);
 
 export default router;
