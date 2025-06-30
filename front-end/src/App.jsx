b52a48e635ac2f9b68ce804b8c3a1e0466f67215
Amel252 <amelghanem25@gmail.com>
2025-06-30 15:51:05 +0200

    Rajout du composant Search permettant le filtre de nos annonces

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -8,9 +8,9 @@ import Signup from "./pages/Signup";
 import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/createListing";
-import UpdateListing from "./pages/UpdateListing"
+import UpdateListing from "./pages/UpdateListing";
 import Listing from "./pages/Listing";
-
+import Search from "./pages/Search";
 
 function App() {
     return (
@@ -22,12 +22,16 @@ function App() {
                 <Route path="/sign-in" element={<Signin />} />
                 <Route path="/sign-up" element={<Signup />} />
                 <Route path="/about" element={<About />} />
-                <Route path="/listing/:listingId" element={<Listing/>}/>
+                <Route path="/search" element={<Search />} />
+                <Route path="/listing/:listingId" element={<Listing />} />
 
                 <Route element={<PrivateRoute />}>
                     <Route path="/profile" element={<Profile />} />
                     <Route path="/create-listing" element={<CreateListing />} />
-                    <Route path="/update-listing/:listingId" element={<UpdateListing />} />
+                    <Route
+                        path="/update-listing/:listingId"
+                        element={<UpdateListing />}
+                    />
                 </Route>
             </Routes>
         </BrowserRouter>
