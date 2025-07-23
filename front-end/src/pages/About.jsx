351e3cd7335852daa84afa1dbf4bee4fb4502823
Amel252 <amelghanem25@gmail.com>
2025-07-23 12:35:27 +0200

    Rajout contexte de a props

--- front-end/src/pages/About.jsx
+++ front-end/src/pages/About.jsx
@@ -1,13 +1,32 @@
-function About() {
+export default function About() {
     return (
-        <div>
-            <h1>Bonjour</h1>
-            <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed
-                ipsum natus recusandae et nam nisi, minus dicta ad iste
-                consequatur cumque sequi molestias!
+        <div className="py-20 px-4 max-w-6xl mx-auto min-h-screen">
+            <h1 className="text-3xl font-bold mb-4 text-slate-800">
+                À propos de l'agence Immo
+            </h1>
+            <p className="mb-4 text-slate-700">
+                Immo Agency est une agence immobilière de premier plan,
+                spécialisée dans l'achat, la vente et la location de biens
+                immobiliers dans les quartiers les plus recherchés. Notre équipe
+                d'agents expérimentés s'engage à offrir un service exceptionnel
+                et à simplifier au maximum le processus d'achat et de vente.
+            </p>
+            <p className="mb-4 text-slate-700">
+                Notre mission est d'aider nos clients à atteindre leurs
+                objectifs immobiliers en leur offrant des conseils d'experts, un
+                service personnalisé et une connaissance approfondie du marché
+                local. Que vous souhaitiez acheter, vendre ou louer un bien,
+                nous sommes là pour vous accompagner à chaque étape.
+            </p>
+            <p className="mb-4 text-slate-700">
+                Notre équipe d'agents possède une vaste expérience et une
+                connaissance approfondie du secteur immobilier, et nous nous
+                engageons à offrir un service de la plus haute qualité à nos
+                clients. Nous sommes convaincus que l'achat ou la vente d'un
+                bien immobilier doit être une expérience passionnante et
+                enrichissante, et nous nous engageons à faire de ce rêve une
+                réalité pour chacun de nos clients.
             </p>
         </div>
     );
 }
-export default About;
